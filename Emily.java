79bd2e172bf69b8ed8f145326c9374f1e8594609
Emily Samuel <emisam26@bergen.org>
2025-12-05 13:40:29 -0500

    Emily

---
+++ Emily.java
@@ -0,0 +1,26 @@
+public class Emily {
+    // -month:String
+    // -day:int
+    // -gift:String
+    // _____________
+    // +Constructors(gift:Striing)
+    // +getGift():String
+    // +setGift(gift:String):void
+    // +toString():String //print your name + birth month + day+ and gift 
+    private String month;
+    private int day;
+    private String gift;
+
+    public Emily(String gift) {
+        this.gift = gift;
+    }
+    public String getGift() {
+        return gift;
+    }
+    public void setGift(String gift) {
+
+    }
+    public String toString() {
+
+    }
+}
