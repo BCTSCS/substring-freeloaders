b311873b5984eabc82c1d353aeb7056f948137f7
Emily Samuel <emisam26@bergen.org>
2025-12-05 13:49:11 -0500

    Emily.java changes

--- Emily.java
+++ Emily.java
@@ -1,26 +1,24 @@
 public class Emily {
-    // -month:String
-    // -day:int
-    // -gift:String
-    // _____________
-    // +Constructors(gift:Striing)
-    // +getGift():String
-    // +setGift(gift:String):void
-    // +toString():String //print your name + birth month + day+ and gift 
     private String month;
     private int day;
     private String gift;
 
-    public Emily(String gift) {
+    public Emily(String gift, int day, String month) {
         this.gift = gift;
+        this.day = day;
+        this.month = month;
+    }
+    public Emily() {
+        this.day = 3;
+        this.month = "October";
     }
     public String getGift() {
         return gift;
     }
     public void setGift(String gift) {
-
+        this.gift = "Book";
     }
     public String toString() {
-
+        return "Emily:" + month + " " + day + " - " + gift;
     }
 }
