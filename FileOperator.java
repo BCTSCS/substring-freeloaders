cdadd80afbb9cc5f7823ee40bef21c287602a20a
Emily Samuel <emisam26@bergen.org>
2025-12-09 14:07:15 -0500

    MP2 DN11

---
+++ FileOperator.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class FileOperator {
+    private static File myFile; // The File containing the data
+    private static Scanner fileReader;
+    
+    public static void createFile(String filename){
+        try {
+            myFile = new File(filename);
+            fileReader = new Scanner (myFile);
+        } catch (IOException e) {
+            System.out.println("File does not exist.");
+        }finally {
+            System.out.println("Continue");
+        }
+    }
+    
+    public static ArrayList<String> getStringData(String filename){
+        createFile(filename);
+        ArrayList<String> result = new ArrayList<>();
+        while (fileReader.hasNextLine()){
+            result.add(fileReader.nextLine());
+        }
+        return result;
+    }
+}
---
+++ Poem.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Poem {
+    private String title;              // The title of the poem
+    private ArrayList<String> lines;   // The lines of the poem
+    public Poem(String title, ArrayList<String> lines){
+
+    }
+    public String toString(){
+
+    }
+}
