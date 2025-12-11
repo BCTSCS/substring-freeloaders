d54294ec0a2e6a559ca4542d29a0cd8208e57c97
Emily Samuel <emisam26@bergen.org>
2025-12-11 17:12:16 -0500

    MP2 CW7

--- SocialMedia.java
+++ SocialMedia.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 
-
 public class SocialMedia {
     private ArrayList<String> posts;
 
@@ -10,11 +9,28 @@ public class SocialMedia {
 
     public int postsWithLength(int minLength){
         int total = 0;
+        for (String post:posts){
+            if (post.length()>=minLength){
+                total++;
+            }
+        }
         return total;
     }
 
     public ArrayList<String> postsWithHashtag (String hashtag){
         ArrayList<String> result = new ArrayList<>();
+        for (String post: posts) {
+            if (post.contains(hashtag)){
+                result.add(post);
+            }
+        }
         return result;
+    }
+
+    public static void main(String[] args) {
+        SocialMedia media1 = new SocialMedia("sample1.txt");
+        // SocialMedia media2 = new SocialMedia("sample2.txt");
+        System.out.println(media1.postsWithHashtag("Java"));
+        System.out.println(media1.postsWithLength(20));
     }
 }
